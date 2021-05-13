b8090aceb1b5b04b6a340168b8c371d3cc171333
yiwang2 <lixinandme@gmail.com>
2021-05-13 15:39:00 +0100

    Update maxSideLength.go

--- maxSideLength.go
+++ maxSideLength.go
@@ -1,4 +1,8 @@
 //1292. Maximum Side Length of a Square with Sum Less than or Equal to Threshold
+/*
+Given a m x n matrix mat and an integer threshold. 
+Return the maximum side-length of a square with a sum less than or equal to threshold or return 0 if there is no such square.
+*/
 package main
 
 import (
